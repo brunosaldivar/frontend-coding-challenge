e2554515aeb2ace60aa65d096bb4eeb8b243b75a
Bruno Saldivar <bruno@saldivar.com.ar>
2022-01-31 21:25:00 -0300

    readme

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,15 +1,6 @@
 export const environment = {
   production: false,
   debugging: false,
-  table : {
-        columns: [
-            {
-                name:"PAYROLL PROVIDER",
-                workerCount : "WORKER"
-            }
-        ]
-
-    },
-    apiEndpoint: "http://192.168.0.107:6502/application/labourstats",
-    fakeData: true
+  apiEndpoint: "http://192.168.0.107:6502/application/labourstats",
+  fakeData: true
 };
